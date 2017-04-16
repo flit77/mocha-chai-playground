a5f479feb64432439e2b5a858e8b7892d86f95db
Vasiliy Shubko <flit77@gmail.com>
2017-04-16 23:29:50 +0200

    successfull post test

--- test/book.js
+++ test/book.js
@@ -1,26 +1,21 @@
-//During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 
 let mongoose = require("mongoose");
 let Book = require('../app/models/book');
 
-//Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
 
 chai.use(chaiHttp);
-//Our parent block
+
 describe('Books', () => {
-    beforeEach((done) => { //Before each test we empty the database
+    beforeEach((done) => {
         Book.remove({}, (err) => { 
            done();         
         });     
     });
-/*
-  * Test the /GET route
-  */
   describe('/GET book', () => {
       it('it should GET all the books', (done) => {
         chai.request(server)
@@ -33,7 +28,6 @@ describe('Books', () => {
             });
       });
   });
-
   /*
   * Test the /POST route
   */
@@ -56,6 +50,26 @@ describe('Books', () => {
               done();
             });
       });
-
+      it('it should POST a book ', (done) => {
+        let book = {
+            title: "The Lord of the Rings",
+            author: "J.R.R. Tolkien",
+            year: 1954,
+            pages: 1170
+        }
+        chai.request(server)
+            .post('/book')
+            .send(book)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message').eql('Book successfully added!');
+                res.body.book.should.have.property('title');
+                res.body.book.should.have.property('author');
+                res.body.book.should.have.property('pages');
+                res.body.book.should.have.property('year');
+              done();
+            });
+      });
   });
-});      
+});
